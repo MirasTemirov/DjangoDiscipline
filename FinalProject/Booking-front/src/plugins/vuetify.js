f8ec9fc258db97f9bc5d7de8f3265a1e733d2413
MirasTemirov <115141212+MirasTemirov@users.noreply.github.com>
2024-05-23 13:14:34 +0500

    Create vuetify.js

---
+++ FinalProject/Booking-front/src/plugins/vuetify.js
@@ -0,0 +1,22 @@
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
+
+import ru from "vuetify/es5/locale/ru";
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  lang: {
+    locales: { ru },
+    current: "ru",
+  },
+  /*
+  theme: {
+    themes: {
+      light: {
+        primary: "#304FFE",
+      },
+    },
+  },
+  */
+});

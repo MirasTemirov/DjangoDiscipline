6c7716b5202253ab1a52d1ca6360314f10f95762
MirasTemirov <115141212+MirasTemirov@users.noreply.github.com>
2024-05-23 13:13:02 +0500

    Create MainLayout.vue

---
+++ FinalProject/Booking-front/src/layouts/MainLayout.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Menu />
+
+    <v-main class="page-container">
+      <v-container fluid class="pa-6">
+        <router-view />
+      </v-container>
+    </v-main>
+  </div>
+</template>
+
+<script>
+import Menu from "../components/Menu";
+
+export default {
+  name: "MainLayout",
+  components: {
+    Menu,
+  },
+};
+</script>
+
+<style scoped></style>
